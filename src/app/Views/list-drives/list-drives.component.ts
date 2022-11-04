ab5f538034c2874517c16f3f1e7b0ff78b8012b5
mohammedhashim790 <55325133+mohammedhashim790@users.noreply.github.com>
2022-11-04 17:36:40 +0530

    First Psuh

---
+++ src/app/Views/list-drives/list-drives.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-drives',
+  templateUrl: './list-drives.component.html',
+  styleUrls: ['./list-drives.component.css']
+})
+export class ListDrivesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainComponent } from './Views/main/main.component';
+import { ListDrivesComponent } from './Views/list-drives/list-drives.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListDrivesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
